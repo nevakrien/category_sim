78b0bace7ae5799c43412cde9cf605a0ad3a6f7e
nevakrien <nevo.krien@gmail.com>
2025-03-30 00:47:04 +0300

    even more validation

--- category.h
+++ category.h
@@ -73,7 +73,7 @@ static inline Element* revive_elem(Category* cat,Element ref){
 }
 
 static inline void delete_elem(Category* cat,ID id) {
-	ASSERT(id.global_id >0 && 0<=cat->elements.data[id.slot].id.slot);
+	ASSERT(id.global_id >0 && 0<=cat->elements.data[id.slot].id.slot && 0<cat->elements.data[id.slot].id.global_id);
 	APPEND(&cat->free_list,id.slot);
 	cat->elements.data[id.slot].id = (ID){.slot = -1,.global_id = -1};
 }
